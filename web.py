cb729410fc8059d8e4f8ffa886f914a289a13614
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-19 02:16:34 +0700

    ok

--- web.py
+++ web.py
@@ -92,7 +92,7 @@ def main():
 
                 # Show Lofi converted audio
                 st.write("Lofi Converted Audio (Preview)")
-                st.audio(music.msc_to_mp3_inf(output_file), format="audio/mp3")
+                st.audio(music.convert_to_mp3(output_file, selected_bitrate), format="audio/mp3")
 
                 st.download_button("Download MP3", music.msc_to_mp3_inf(output_file), song_name+"_lofi.mp3")
     except:
