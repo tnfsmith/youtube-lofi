aca2f277cdfd54783c1bcf47bdfb3fc92efb190b
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-19 13:46:57 +0700

    ok

--- web.py
+++ web.py
@@ -100,17 +100,18 @@ def main():
         st.warning("Error Try again")
 
     # Footer and BuyMeACoffee button
-    st.markdown("""
-        <h10 style="text-align: center; position: fixed; bottom: 3rem;">Give a ⭐ on <a href="https://github.com/tnfsmith/"> Github</a> </h10>""",
+    # Footer and BuyMeACoffee button
+st.markdown("""
+        <h10 style="text-align: center; position: fixed; bottom: 3rem;">Developed <a href='https://lequocthai.com'>Lê Quốc Thái</a> | <a href='mailto:lequocthai@gmail.com'>lequocthai[at]gmail.com</a> | <a href='https://t.me/tnfsmith'>Telegram</a> | <a href='tel:0985010707'>Zalo</a> </h10>""",
         unsafe_allow_html=True)
-    button = """<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="LeQuocThaiy" data-color="#FFDD00" data-emoji="📖" data-font="Cookie" data-text="Buy me a Coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>"""
-    html(button, height=70, width=220)
-    st.markdown(
+button = """<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="LeQuocThaiy" data-color="#FFDD00" data-emoji="🥤" data-font="Cookie" data-text="Buy me a Coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>"""
+html(button, height=70, width=225)
+st.markdown(
         """
         <style>
-            iframe[width="220"] {
+            iframe[width="225"] {
                 position: fixed;
-                bottom: 60px;
+                bottom: 35px;
                 right: 40px;
             }
         </style>
