971a2388a50fbe58bca9df22036d6ae674ad184f
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-19 16:33:36 +0700

    ok

--- web.py
+++ web.py
@@ -92,7 +92,7 @@ def main():
                 st.audio(mp3_base_file, format="audio/mp3")
                 # Download button for the original audio
                 st.download_button(
-                #    label="💾 Download Original Youtube Audio 🎵",
+                    label="💾 Download Original Youtube Audio 🎵",
                     data=mp3_base_file,
                     file_name=f"{song_name}.mp3",
                     mime="audio/mp3"
