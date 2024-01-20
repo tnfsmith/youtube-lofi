21807aac1746e0d139407208fc687054ece2ce35
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-20 11:51:54 +0700

    ok

--- webui.py
+++ webui.py
@@ -100,7 +100,7 @@ def main():
         st.audio(mp3_base_file, format="audio/mp3")
 
         # Get user settings for slowedreverb function
-        room_size, damping, wet_level, dry_level, delay, slow_factor = get_user_settings()
+        #room_size, damping, wet_level, dry_level, delay, slow_factor = get_user_settings()
         #duration =0        
         if duration <= 1200:  # 20 minutes
             room_size, damping, wet_level, dry_level, delay, slow_factor = get_user_settings()
