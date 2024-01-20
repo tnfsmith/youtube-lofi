f3a3d64679b9e042dc88d8c040bec1940147c7aa
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-20 23:37:57 +0700

    ok

--- webui.py
+++ webui.py
@@ -63,8 +63,8 @@ def download_youtube_audio(youtube_link):
 st.set_page_config(page_title="Youtube Audio Download & Lofi Converter", page_icon=":microphone:", layout="wide")
 def main():
     
-    st.title(":microphone: Youtube Audio Download & Lofi Converter (Lossless)")
-    st.info("🌟 Auto download audio at 320kbps. Videos download is still in development. 🎉 Tip: Use Headphones for the best experience :headphones:")
+    st.title(":microphone: Youtube Audio Download & Lofi Converter (Lossless)🎵")
+    st.info("🌟 Auto download audio at 320kbps 🎵. Videos download is still in development. 🎉 Tip: Use Headphones for the best experience :headphones:")
 
     # Initialize session state
     if 'audio_data' not in st.session_state:
