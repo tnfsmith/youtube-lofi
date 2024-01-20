5ccac3f54400a422c2cfe9800bebeb2b427ea601
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-20 22:57:06 +0700

    ok

--- webui.py
+++ webui.py
@@ -76,7 +76,7 @@ def main():
     duration = 0
     with st.form(key='youtube_link_form'):
         youtube_link = st.text_input("🔎 Paste the YouTube link 🔗 to download/convert Lofi:", value="https://www.youtube.com/watch?v=JxBnLmCOEJ8", help="Defaul URL is ==> Ai muốn nghe không - Đen Vâu")
-        col1, col2 = st.column(2)
+        col1, col2 = st.columns(2)
         with col1:
             submit_button = st.form_submit_button(label='💯 Click Process Audio 🔃')
         with col2:
