2eda9f40d4b561eaf293bcdeaac15d9ca025777d
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-02-28 19:48:33 +0700

    Update webui.py

--- webui.py
+++ webui.py
@@ -109,7 +109,7 @@ def main():
         # Get user settings for slowedreverb function
         #room_size, damping, wet_level, dry_level, delay, slow_factor = get_user_settings()
         #duration =0        
-        if duration <= 7200:  # 120 minutes
+        if duration <= 8000:  # 133 minutes
                 room_size, damping, wet_level, dry_level, delay, slow_factor = get_user_settings()
                 if  st.session_state.reverb_settings != (room_size, damping, wet_level, dry_level, delay, slow_factor):
                     st.session_state.reverb_settings = (room_size, damping, wet_level, dry_level, delay, slow_factor)
